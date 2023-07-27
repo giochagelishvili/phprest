f1bd37f86ae5c49a3838ca0964257246799fb820
Giorgi Chagelishvili <chagx116@gmail.com>
2023-07-27 12:10:41 +0400

    create single post reader

--- models/Post.php
+++ models/Post.php
@@ -23,8 +23,33 @@ class Post {
     public function read() {
         // SQL
         $sql = "SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at FROM " . $this->table . " p LEFT JOIN categories c ON p.category_id = c.id ORDER BY p.created_at DESC";
+        
         $stmt = $this->conn->prepare($sql);    
+        
         $stmt->execute();
+        
         return $stmt;
+    }
+
+    // Get Single Post
+    public function read_single() {
+        // SQL
+        $sql = "SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at FROM " . $this->table . " p LEFT JOIN categories c ON p.category_id = c.id WHERE p.id = ? LIMIT 0,1";
+        
+        $stmt = $this->conn->prepare($sql);
+
+        // Bind ID
+        $stmt->bindParam(1, $this->id);
+
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // Set properties
+        $this->title = $row["title"];
+        $this->body = $row["body"];
+        $this->author = $row["author"];
+        $this->category_id = $row["category_id"];
+        $this->category_name = $row["category_name"];
     }
 }
