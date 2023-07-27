c8a4457dda07c679132e8cb990830607499d45e6
Giorgi Chagelishvili <chagx116@gmail.com>
2023-07-27 12:00:12 +0400

    create reader

--- models/Post.php
+++ models/Post.php
@@ -22,21 +22,7 @@ class Post {
     // Get Posts
     public function read() {
         // SQL
-        $sql = "SELECT 
-                    c.name as category_name, 
-                    p.id, 
-                    p.category_id, 
-                    p.title, 
-                    p.body, 
-                    p.author, 
-                    p.created_at 
-                FROM 
-                    ' . $this->table . ' p 
-                LEFT JOIN 
-                    categories c ON p.category_id = c.id
-                ORDER BY
-                    p.created_at DESC";
-        
+        $sql = "SELECT c.name as category_name, p.id, p.category_id, p.title, p.body, p.author, p.created_at FROM " . $this->table . " p LEFT JOIN categories c ON p.category_id = c.id ORDER BY p.created_at DESC";
         $stmt = $this->conn->prepare($sql);    
         $stmt->execute();
         return $stmt;
